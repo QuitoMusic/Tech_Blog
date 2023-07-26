3ff9690db752245baeb4c085066fe9de95f25ce7
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-26 13:21:51 -0400

    User login, logout, creatin and edit routes with api functionality/ added the link to the index.js file containing routes from controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
 // Import routes 
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 
+router.use('/api', apiRoutes);
 // middleware to handle home-related requests
 router.use('/', homeRoutes);
 // middleware to handle dashboard-related requests
