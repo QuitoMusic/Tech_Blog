8cd501be9b4048cb087085ecd8f2bcf26e59e2c9
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-26 14:59:23 -0400

    Deleted function for plurals, will stick to date formatting on helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,14 +3,6 @@ const format_date = date => {
   return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
 };
 
-// Function to format a word into its plural form if the amount is not equal to 1
-const format_plural = (word, amount) => {
-  if (amount !== 1) {
-    return `${word}s`;
-  }
-  return word;
-};
-
 module.exports = {
   format_date: format_date,
   format_plural: format_plural
